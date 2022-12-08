b95b1f38f8f338a9d4a493b448b42ee95f67583c
max <max.taran.210@mail.ru>
2022-12-08 14:10:48 +0300

    changes 2

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 codes = []
 last_id = 0
+rooms = {}
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/getcode", methods=['POST'])
+@app.route("/strgame", methods=['POST'])
 def getcode():
     global last_id
     last_id += 1
     codes.append(last_id)
+    rooms.update({last_id: {'last_word': 'кот'}})
 
-    return "Code:"+str(last_id)
+    return redirect('game.html')
 
 
 if __name__ == "__main__":
