741b2fb644e2694d66625e971c2166f4514b5c82
Vladimir Papazov <papazovvladimir88@gmail.com>
2022-12-09 10:23:18 +0300

    dsdasa

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ last_id = '0'
 rooms = {'1': {'words': ['кот'], 'users': [], 'host': 'макс'}}
 sessions = {}
 
+words2 = ["кот", "собака", "арбуз", "мандарин"]
+
 errors = {
     '100': 'Игрок уже существует!',
     '101': 'Сессия была закрыта!',
-    '102': 'Не правильный код комнаты'
+    '102': 'Не правильный код комнаты',
+    '103': 'Неправильное слово'
 }
 
 
@@ -46,8 +49,10 @@ def close(session):
 @app.route("/start", methods=['POST', 'GET'])
 def start():
     global last_id
-    last_id = ''.join([random.choice(string.ascii_lowercase+string.digits) for _ in range(10)])
-    rooms.update({last_id: {'words': ['кот'], 'users': [], 'host': request.args["name"]}})
+    last_id = ''.join(
+        [random.choice(string.ascii_lowercase+string.digits) for _ in range(10)])
+    rooms.update(
+        {last_id: {'words': [random.choice(words2)], 'users': [], 'host': request.args["name"]}})
 
     return redirect(f'/login?code={last_id}&name={request.args["name"]}')
 
@@ -56,7 +61,8 @@ def start():
 def login():
     code = request.args['code']
     name = request.args['name']
-    session_id = ''.join([random.choice(string.ascii_lowercase+string.digits) for _ in range(10)])
+    session_id = ''.join(
+        [random.choice(string.ascii_lowercase+string.digits) for _ in range(10)])
     sessions.update({name: session_id})
     try:
         room = rooms[code]
@@ -74,8 +80,15 @@ def game(code):
     name = request.args['name']
     session = request.args['session']
     if request.args.get('message', False):
-        rooms[code]["words"].append(request.args.get('message', False))
-        return redirect(f'/game/{code}?name={name}&session={session}')
+        word = request.args.get('message', False)
+        lastword = rooms[code]["words"][-1]
+
+        if ((word[0].lower() == lastword[-1].lower()) and (word.lower() not in rooms[code]["words"])):
+            rooms[code]["words"].append(
+                request.args.get('message', False).lower())
+            return redirect(f'/game/{code}?name={name}&session={session}')
+        else:
+            return redirect(f"/game/{code}?name={name}&session={session}&error=103")
     try:
         room = rooms[code]
     except KeyError:
@@ -84,7 +97,8 @@ def game(code):
     try:
         if sessions[name] == session:
             return render_template('game.html', code=code, name=name, users=room['users'], words=room['words'],
-                                   session=session)
+                                   session=session,
+                                   error=errors[request.args['error']] if request.args.get('error', False) else '')
         else:
             return redirect('/')
     except KeyError:
